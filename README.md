1ebaae2abf1be91169e11c56efb2138974b2e2c7
Alejandro Jazmatie <alejandro.jaz92@gmail.com>
2019-07-26 11:31:25 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## App del clima - Curso Node
+
+Recuerden ejecutar el siguiente comando para instalar las librerias
+
+```
+npm install
+```
+
+### Ejemplo
+
+```
+node app -d "Rosario"
+```
---
+++ app.js
@@ -0,0 +1,28 @@
+const argv = require('./config/yargs').argv;
+
+const lugar = require('./lugar/lugar');
+const clima = require('./clima/clima');
+
+// lugar.getLugarLatLng(argv.direccion)
+//     .then(console.log);
+
+// clima.getClima('-32.919998', '-60.820000')
+//     .then(console.log)
+//     .catch(console.log);
+
+const getInfo = async(direccion) => {
+
+    try {
+        const coords = await lugar.getLugarLatLng(direccion);
+        const temp = await clima.getClima(coords.lat, coords.lng);
+        return `El clima de ${ coords.direccion } es de ${ temp}ºc`;
+
+    } catch (e) {
+        return `No se pudo obtener el clima de ${ direccion }`;
+    }
+}
+
+
+getInfo(argv.direccion)
+    .then(console.log)
+    .catch(console.log);
